931b9dfa5704d4a61b3e1d417331fe3f1bfc5fd2
JohnMVan <john.vanhessche@gmail.com>
2023-01-07 14:55:27 -0600

    Web 430 Presentation 1.2

--- public/js/button.js
+++ public/js/button.js
@@ -13,19 +13,20 @@ export class Button {
             style="background-color: aqua; color: black;"`
         
         if(page==="about-button") {
-            buttonString += `href="./about.html">About`
+            buttonString += `href="/personal-portfolio/about.html">About`
         } else if(page==="bucket-button") {
-            buttonString += `href="./bucket-list.html">Bucket List`
+            buttonString += `href="/personal-portfolio/bucket-list.html">Bucket List`
         } else if(page==="testing-button") {
-            buttonString += `href="./api-test.html">API Unit Testing`
+            buttonString += `href="/personal-portfolio/api-test.html">API Unit Testing`
         } else if(page==="diagrams-button") {
-            buttonString += `href="./database.html">Database Diagrams`
+            buttonString += `href="/personal-portfolio/database.html">Database Diagrams`
         } else if(page==="resume-button") {
-            buttonString += `href="./resume.html">Resume`
+            buttonString += `href="/personal-portfolio/resume.html">Resume`
         } else if(page==="projects-button") {
-            buttonString += `href="projects.html">Projects`
+            buttonString += `href="/personal-portfolio/projects.html">Projects`
+        } else if(page==="presentation-button") {
+            buttonString += `href="././web-430/vanhessche-tech-value-stream.html">Presentations`
         }
-        
         
 
         return buttonString + `</a></button>`;
--- public/js/header.js
+++ public/js/header.js
@@ -12,13 +12,14 @@ class Header extends HTMLElement
                 </div>
 
                 <div i="nav-container">
-                <button><a class="navlinks" id="home-button" href="./index.html">Home</a></button>
-                <button><a class="navlinks" id="about-button" href="./about.html">About</a></button>
-                <button><a class="navlinks" id="resume-button" href="./resume.html">Resume</a></button>
-                <button><a class="navlinks" id="projects-button" href="./projects.html">Projects</a></button>
-                <button><a class="navlinks" id="diagrams-button" href="./database.html">Database Diagrams</a></button>
-                <button><a class="navlinks" id="testing-button" href="./api-test.html">API Unit Testing</a></button>
-                <button><a class="navlinks" id="bucket-button" href="./bucket-list.html">Bucket List</a></button>
+                <button><a class="navlinks" id="home-button" href="/personal-portfolio/index.html">Home</a></button>
+                <button><a class="navlinks" id="about-button" href="/personal-portfolio/about.html">About</a></button>
+                <button><a class="navlinks" id="resume-button" href="/personal-portfolio/resume.html">Resume</a></button>
+                <button><a class="navlinks" id="projects-button" href="/personal-portfolio/projects.html">Projects</a></button>
+                <button><a class="navlinks" id="diagrams-button" href="/personal-portfolio/database.html">Database Diagrams</a></button>
+                <button><a class="navlinks" id="testing-button" href="/personal-portfolio/api-test.html">API Unit Testing</a></button>
+                <button><a class="navlinks" id="bucket-button" href="/personal-portfolio/bucket-list.html">Bucket List</a></button>
+                <button><a class="navlinks" id="presentation-button" href="././web-430/vanhessche-tech-value-stream.html">Presentations</a></button>
                 </div> 
 
             </div>
